6ac51a94070e33dacbc4d4895f35e77aa1f19493
JorgeGarciaRa <>
2020-12-21 12:20:43 -0600

    Update Data.js

--- RecetarioDeCocina/components/Data.js
+++ RecetarioDeCocina/components/Data.js
@@ -1,53 +1,118 @@
 export const data = {
-    recipes: [
-        id: '1',
-        name: 'Pizza',
-        portions: '3',
-        ruta: require('../images/pizza.jpg'),
-        category: 'TRENDING',
-        ingredients: [
-          {
-            ingredient: 'peperoni',
-            quantity: '200 g',
-          },
-          {
-            ingredient: 'jamon',
-            quantity: '200 g',
-          },
-          {
-            ingredient: 'harina',
-            quantity: '1 Kg',
-          },
-        ],
-      },
+  recipes: [
+    {
+      id: '1',
+      name: 'Pizza',
+      portions: '3',
+      ruta: require('../images/pizza.jpg'),
+      category: 'TRENDING',
+      ingredients: [
+        {
+          ingredient: 'peperoni',
+          quantity: '200 g',
+        },
+        {
+          ingredient: 'jamon',
+          quantity: '200 g',
+        },
+        {
+          ingredient: 'harina',
+          quantity: '1 Kg',
+        },
+      ],
+    },
 
-      {
-        id: '2',
-        name: 'Pollo Enchipotlado',
-        portions: '1',
-        ruta: require('../images/pollo.jpg'),
-        category: 'TRENDING',
-        ingredients: [
-            {
-            ingredient: 'pollo',
-             quantity: '2 kg',
-              },
+    {
+      id: '2',
+      name: 'Pollo Enchipotlado',
+      portions: '1',
+      ruta: require('../images/pollo.jpg'),
+      category: 'TRENDING',
+      ingredients: [
           {
-            ingredient: 'chile',
-            quantity: '250 g',
-          },
-          {
-            ingredient: 'cebolla',
-            quantity: '1 pz',
-          },
-          {
-            ingredient: 'papas',
-            quantity: '4 pz',
-          },
-       
-        ],
-      },
-
-   
-    ],
-  };
+          ingredient: 'pollo',
+           quantity: '2 kg',
+            },
+        {
+          ingredient: 'chile',
+          quantity: '250 g',
+        },
+        {
+          ingredient: 'cebolla',
+          quantity: '1 pz',
+        },
+        {
+          ingredient: 'papas',
+          quantity: '4 pz',
+        },
+     
+      ],
+    },
+    {
+      id: '3',
+      name: 'Pastel',
+      portions: '5',
+      ruta: require('../images/pastel.jpg'),
+      category: 'TRENDING',
+      ingredients: [
+        {
+          ingredient: 'Huevos',
+          quantity: '10 pz',
+        },
+        {
+          ingredient: 'crema',
+          quantity: '20 g',
+        },
+        {
+          ingredient: 'harina',
+          quantity: '1 Kg',
+        },
+      ],
+    },
+    {
+      id: '4',
+      name: 'Ensalada de Zanahoria',
+      portions: '5',
+      ruta: require('../images/ensalada.jpg'),
+      category: 'TRENDING',
+      ingredients: [
+        {
+          ingredient: 'zanahorias',
+          quantity: '5 pz',
+        },
+        {
+          ingredient: 'nuez',
+          quantity: '20 g',
+        },
+        {
+          ingredient: 'pasas',
+          quantity: '1 Kg',
+        },
+        
+      ],
+    },
+    {
+      id: '5',
+      name: 'Tostadas',
+      portions: '3',
+      ruta: require('../images/tostadas.jpg'),
+      category: 'TRENDING',
+      ingredients: [
+        {
+          ingredient: 'tostada',
+          quantity: '15 pz',
+        },
+        {
+          ingredient: 'tomate',
+          quantity: '20 g',
+        },
+        {
+          ingredient: 'aguacate',
+          quantity: '1 Kg',
+        },
+        
+      ],
+    },
+ 
+  ],
+};
